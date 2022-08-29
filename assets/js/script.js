0188e579c3bfebe8858b24613f6e20a2213a9a86
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-08-28 23:26:59 -0400

    fixed UV index colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,44 @@
 
 var apiKey = "6015d4614214e35f89f83b4825650637";
 var currentWeatherEl = document.querySelector("#current-weather");
-var currentCityEl = document.querySelector(".current-city");
+var currentCityEl = document.querySelector("#current-city");
 var currentWeatherIconEl = document.querySelector("#current-weather-icon")
-var currentTempEl = document.querySelector(".current-temp");
-var currentHumEl = document.querySelector(".current-humidity");
-var currentWindEl = document.querySelector(".current-wind");
+var currentTempEl = document.querySelector("#current-temp");
+var currentHumEl = document.querySelector("#current-humidity");
+var currentWindEl = document.querySelector("#current-wind");
+var currentUvEl = document.querySelector("#current-uv");
+var currentUvValueEl = document.querySelector("#current-uv-value");
 var searchHistory = [];
 
 var searchFormEl = document.querySelector('#search-form');
 
-function getCurrentWeather(query) {
+function getCurrentWeather(city) {
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + query + "&appid=" + apiKey)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + "&appid=" + apiKey + "&units=imperial")
 
         .then(function (response) {
             return response.json();
         })
 
         .then(function (response) {
-            var longitude = response.coord.lon;
             var latitude = response.coord.lat;
+            var longitude = response.coord.lon;
+            var cityName = response.name;
+            var currentDate = moment().format("M/D/YYYY");
+            currentIcon = response.weather[0].icon;
+            var currentTemp = response.main.temp;
+            var currentHumidity = response.main.humidity;
+            var windSpeed = response.wind.speed;
+            var country = response.sys.country;
 
-            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey)
+            currentCityEl.textContent = cityName + " (" + currentDate + ")";
+            currentWeatherIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentIcon + ".png");
+            currentTempEl.textContent = "Temp: " + Math.round(currentTemp) + " \u00B0F";
+            currentWindEl.textContent = "Wind: " + windSpeed + " MPH";
+            currentHumEl.textContent = "Humidity: " + currentHumidity + "%";
+            currentWeatherEl.setAttribute("style", "border: 1px solid black");
+
+            fetch("https://api.openweathermap.org/data/2.5/uvi?q=" + city + "&appid=" + apiKey + "&lat=" + latitude + "&lon=" + longitude)
 
                 .then(function (response) {
                     return response.json();
@@ -30,26 +46,25 @@ function getCurrentWeather(query) {
 
                 .then(function (response) {
                     console.log(response);
-                    var cityName = response.name;
-                    var currentDate = moment().format("M/D/YYYY");
-                    currentIcon = response.weather[0].icon;
-                    var currentTemp = response.main.temp;
-                    var currentHumidity = response.main.humidity;
-                    var windSpeed = response.wind.speed;
-                    var country = response.sys.country;
+                    var currentUv = response.value;
 
-                    currentCityEl.textContent = cityName + " (" + currentDate + ")";
-                    currentWeatherIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentIcon + ".png");
-                    currentTempEl.textContent = "Temp: " + Math.round(currentTemp) + " \u00B0F";
-                    currentWindEl.textContent = "Wind: " + windSpeed + " MPH";
-                    currentHumEl.textContent = "Humidity: " + currentHumidity + "%";
-                    currentWeatherEl.setAttribute("style", "border: 1px solid black");
-                })
+                    currentUvEl.textContent = "UV Index: ";
+                    currentUvValueEl.textContent = currentUv;
 
-                .catch(function (error) {
-
-                })
+                    if (currentUv <= 2) {
+                        currentUvValueEl.classList.add("favorable");
+                    } else if (currentUv > 2 && currentUv <= 7) {
+                        currentUvValueEl.classList.add("moderate");
+                    } else {
+                        currentUvValueEl.classList.add("severe");
+                    }
         })
+
+})
+
+        .catch (function (error) {
+
+})
 };
 
 function handleSearchFormSubmit(e) {
@@ -60,9 +75,8 @@ function handleSearchFormSubmit(e) {
         console.log("Invalid Input!");
         return;
     }
+    currentUvValueEl.classList.remove("favorable", "moderate", "severe");
     getCurrentWeather(searchInputVal);
-    console.log(searchInputVal);
-
 
 }
 
